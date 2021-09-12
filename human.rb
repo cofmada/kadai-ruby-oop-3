fe2c61030fdb4b8fab334ed2ffb8bb823293cc3c
cof.iland <shima4113@gmail.com>
2021-09-12 03:54:03 +0000

    add kadai-ruby-oop-3

---
+++ human.rb
@@ -0,0 +1,14 @@
+require './thinkable.rb'
+require './animal.rb'
+
+class Human < Animal
+    
+  include Thinkable
+    
+  attr_accessor :hobby
+    
+  def initialize(name,age,hobby)
+    super(name,age)
+    self.hobby = hobby
+  end
+end
